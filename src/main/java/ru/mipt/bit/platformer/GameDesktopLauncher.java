c30a38478eec30a6653c79f1b4528d48c30d60dc
NikitaRR <nikita200993@gmail.com>
2021-10-06 00:05:49 +0300

    launcher

--- src/main/java/ru/mipt/bit/platformer/GameDesktopLauncher.java
+++ src/main/java/ru/mipt/bit/platformer/GameDesktopLauncher.java
@@ -1,137 +1,30 @@
 package ru.mipt.bit.platformer;
 
-import com.badlogic.gdx.ApplicationListener;
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.maps.MapRenderer;
-import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
-import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.math.GridPoint2;
-import com.badlogic.gdx.math.Interpolation;
-import com.badlogic.gdx.math.Rectangle;
-import ru.mipt.bit.platformer.util.TileMovement;
+import ru.mipt.bit.platformer.driver.GameDriver;
+import ru.mipt.bit.platformer.logic.GameLogic;
+import ru.mipt.bit.platformer.logic.Player;
 
-import static com.badlogic.gdx.graphics.GL20.GL_COLOR_BUFFER_BIT;
-import static ru.mipt.bit.platformer.util.GdxGameUtils.createBoundingRectangle;
-import static ru.mipt.bit.platformer.util.GdxGameUtils.createSingleLayerMapRenderer;
-import static ru.mipt.bit.platformer.util.GdxGameUtils.drawTextureRegionUnscaled;
-import static ru.mipt.bit.platformer.util.GdxGameUtils.getSingleLayer;
-import static ru.mipt.bit.platformer.util.GdxGameUtils.moveRectangleAtTileCenter;
-
-public class GameDesktopLauncher implements ApplicationListener {
-
-    private static final float MOVEMENT_SPEED = 0.4f;
-
-    private final DirectionResolver directionResolver;
-
-    private Batch batch;
-
-    private TiledMap level;
-    private MapRenderer levelRenderer;
-    private TileMovement tileMovement;
-
-    private Texture blueTankTexture;
-    private TextureRegion playerGraphics;
-    private Rectangle playerRectangle;
-    private Player player;
-
-    private Texture greenTreeTexture;
-    private TextureRegion treeObstacleGraphics;
-
-    private GridPoint2 treeObstacleCoordinates = new GridPoint2();
-    private Rectangle treeObstacleRectangle = new Rectangle();
-
-    public GameDesktopLauncher(final DirectionResolver directionResolver) {
-        this.directionResolver = directionResolver;
-    }
+public class GameDesktopLauncher {
 
     public static void main(String[] args) {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
         // level width: 10 tiles x 128px, height: 8 tiles x 128px
         config.setWindowedMode(1280, 1024);
-        new Lwjgl3Application(new GameDesktopLauncher(new DefaultGdxDirectionResolver()), config);
+        try (var gameDriver = createGameDriver()) {
+            new Lwjgl3Application(gameDriver, config);
+        }
     }
 
-    @Override
-    public void create() {
-        batch = new SpriteBatch();
-        // load level tiles
-        level = new TmxMapLoader().load("level.tmx");
-        levelRenderer = createSingleLayerMapRenderer(level, batch);
-        TiledMapTileLayer groundLayer = getSingleLayer(level);
-        tileMovement = new TileMovement(groundLayer, Interpolation.smooth);
-        // Texture decodes an image file and loads it into GPU memory, it represents a native resource
-        blueTankTexture = new Texture("images/tank_blue.png");
-        // TextureRegion represents Texture portion, there may be many TextureRegion instances of the same Texture
-        playerGraphics = new TextureRegion(blueTankTexture);
-        playerRectangle = createBoundingRectangle(playerGraphics);
-        player = new Player(new GridPoint2(1, 1));
-        greenTreeTexture = new Texture("images/greenTree.png");
-        treeObstacleGraphics = new TextureRegion(greenTreeTexture);
-        treeObstacleCoordinates = new GridPoint2(1, 3);
-        treeObstacleRectangle = createBoundingRectangle(treeObstacleGraphics);
-        moveRectangleAtTileCenter(groundLayer, treeObstacleRectangle, treeObstacleCoordinates);
-    }
-
-    @Override
-    public void render() {
-        clearScreen();
-        // get time passed since the last render
-        float deltaTime = Gdx.graphics.getDeltaTime();
-        directionResolver.resolveDirection()
-                .ifPresent(direction -> player.startMove(direction, treeObstacleCoordinates));
-        // calculate interpolated player screen coordinates
-        tileMovement.moveRectangleBetweenTileCenters(
-                playerRectangle,
-                player.getCurrentPosition(),
-                player.getDestinationPosition(),
-                player.getMoveProgress()
+    private static GameDriver createGameDriver() {
+        return new GameDriver(
+                new GameLogic(
+                        new Player(new GridPoint2(1, 1)),
+                        new GridPoint2(1, 3)
+                ),
+                new DefaultGdxDirectionResolver()
         );
-        player.updateProgress(deltaTime, MOVEMENT_SPEED);
-        draw();
-    }
-
-    @Override
-    public void resize(int width, int height) {
-        // do not react to window resizing
-    }
-
-    @Override
-    public void pause() {
-        // game doesn't get paused
-    }
-
-    @Override
-    public void resume() {
-        // game doesn't get paused
-    }
-
-    @Override
-    public void dispose() {
-        // dispose of all the native resources (classes which implement com.badlogic.gdx.utils.Disposable)
-        greenTreeTexture.dispose();
-        blueTankTexture.dispose();
-        level.dispose();
-        batch.dispose();
-    }
-
-    private void clearScreen() {
-        Gdx.gl.glClearColor(0f, 0f, 0.2f, 1f);
-        Gdx.gl.glClear(GL_COLOR_BUFFER_BIT);
-    }
-
-    private void draw() {
-        // render each tile of the level
-        levelRenderer.render();
-        batch.begin();
-        drawTextureRegionUnscaled(batch, playerGraphics, playerRectangle, player.getRotation());
-        drawTextureRegionUnscaled(batch, treeObstacleGraphics, treeObstacleRectangle, 0f);
-        batch.end();
     }
 }
