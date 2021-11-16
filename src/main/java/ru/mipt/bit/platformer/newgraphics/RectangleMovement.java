883408cb0d2dd5a2503aa85e8ddd818cb1549a11
NikitaRR <nikita200993@gmail.com>
2021-11-16 09:35:48 +0300

    simplify

--- src/main/java/ru/mipt/bit/platformer/newgraphics/RectangleMovement.java
+++ src/main/java/ru/mipt/bit/platformer/newgraphics/RectangleMovement.java
@@ -14,18 +14,8 @@ public class RectangleMovement {
     }
 
     public Rectangle moveRectangle(final Rectangle rectangle, final FloatPoint2D floatPoint2D) {
-        final var movedToCenter = moveToCenter(rectangle, floatPoint2D);
-        final float remainderX = floatPoint2D.getX() - (int) floatPoint2D.getX();
-        final float remainderY = floatPoint2D.getY() - (int) floatPoint2D.getY();
-        return movedToCenter.setX(movedToCenter.getX() + tileWidth * remainderX)
-                .setY(movedToCenter.getY() + tileHeight * remainderY);
-    }
-
-    private Rectangle moveToCenter(final Rectangle rectangle, final FloatPoint2D floatPoint2D) {
-        final int logicalX = (int) floatPoint2D.getX();
-        final int logicalY = (int) floatPoint2D.getY();
-        final float rectangleX = logicalX * tileWidth + tileWidth / 2.0f - rectangle.getWidth() / 2.0f;
-        final float rectangleY = logicalY * tileHeight + tileHeight / 2.0f - rectangle.getHeight() / 2.0f;
+        final float rectangleX = floatPoint2D.getX() * tileWidth + tileWidth / 2.0f - rectangle.getWidth() / 2.0f;
+        final float rectangleY = floatPoint2D.getY() * tileHeight + tileHeight / 2.0f - rectangle.getHeight() / 2.0f;
         return rectangle.setX(rectangleX)
                 .setY(rectangleY);
     }
