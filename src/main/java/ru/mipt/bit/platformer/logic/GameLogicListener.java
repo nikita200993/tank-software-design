eb0a83a7474613a334f32004adbe88a7a9b281ab
NikitaRR <nikita200993@gmail.com>
2021-11-16 17:08:04 +0300

    fix signature

--- src/main/java/ru/mipt/bit/platformer/logic/GameLogicListener.java
+++ src/main/java/ru/mipt/bit/platformer/logic/GameLogicListener.java
@@ -3,5 +3,5 @@ package ru.mipt.bit.platformer.logic;
 import java.util.List;
 
 public interface GameLogicListener {
-    void onRegister(List<GameObjectView> tanks, List<GameObjectView> trees);
+    void onRegister(List<? extends GameObjectView> tanks, List<? extends GameObjectView> trees);
 }
